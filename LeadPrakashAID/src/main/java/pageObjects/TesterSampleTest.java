a69200fb8fece97b8afcb64f4e8d1095e4049263
eRisheXpress <eRisheXpress@LAPTOP-MFKDHCQD.myfiosgateway.com>
2021-12-27 21:35:07 -0500

    This is tester Shila commiting and pushing new class...

---
+++ LeadPrakashAID/src/main/java/pageObjects/TesterSampleTest.java
@@ -0,0 +1,10 @@
+package pageObjects;
+
+public class TesterSampleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is sample class by TESTER Shila");
+	}
+
+}
