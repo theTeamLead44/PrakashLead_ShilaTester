84e53edfba40a75c47952bc60c0bb4ea498675e6
eRisheXpress <eRisheXpress@LAPTOP-MFKDHCQD.myfiosgateway.com>
2021-12-27 21:42:56 -0500

    tester Commits message

--- LeadPrakashAID/src/main/java/pageObjects/TesterSampleTest.java
+++ LeadPrakashAID/src/main/java/pageObjects/TesterSampleTest.java
@@ -5,6 +5,7 @@ public class TesterSampleTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is sample class by TESTER Shila");
+		//
 	}
 
 }
