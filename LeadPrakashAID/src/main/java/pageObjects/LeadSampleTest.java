d6e11fd8bb7e4c73d079c09d69b2b9c39752e1c6
eRisheXpress <eRisheXpress@LAPTOP-MFKDHCQD.myfiosgateway.com>
2021-12-27 21:16:53 -0500

    Lead commits this message into gitHub....

---
+++ LeadPrakashAID/src/main/java/pageObjects/LeadSampleTest.java
@@ -0,0 +1,10 @@
+package pageObjects;
+
+public class LeadSampleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is sample class by LEAD Prkaahs");
+	}
+
+}
